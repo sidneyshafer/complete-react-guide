c21fb342011a2956d7393bfe458d3dbd494fcfb7
Sidney Shafer <sidney@mnshafers.com>
2023-06-23 13:06:13 -0500

    Code Commit

---
+++ 09-dummy-login-page/src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+
+import Card from '../UI/Card/Card';
+import Button from '../UI/Button/Button';
+import classes from './Home.module.css';
+import AuthContext from '../../context/auth-context';
+
+const Home = (props) => {
+  const authCxt = useContext(AuthContext);
+
+  return (
+    <Card className={classes.home}>
+      <h1>Welcome back!</h1>
+      <Button onClick={authCxt.onLogout}>Logout</Button>
+    </Card>
+  );
+};
+
+export default Home;
