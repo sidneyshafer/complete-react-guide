4c2ce69bbbbb849408fe7955f88a3ace8e8b0df8
Sidney Shafer <sidney@mnshafers.com>
2023-07-05 06:50:47 -0500

    Code Commit

---
+++ 27-adding-authentication/frontend/src/pages/Root.js
@@ -0,0 +1,40 @@
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
+
+import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
+
+function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  // clear token after 1 hour
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'post' });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' });
+    }, tokenDuration); // 1 hour in miliseconds
+  }, [token, submit]);
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
