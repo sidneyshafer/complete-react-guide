1ddb360f305f4481e9fcb0835b683f1068afa039
Sidney Shafer <sidney@mnshafers.com>
2023-07-07 14:06:40 -0500

    Code Commit

---
+++ 29-nextjs-dummy-news-project/pages/news/[newsId].js
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router';
+
+// domain.com/news/[newsId]
+
+function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  return <h1>The Detail Page</h1>;
+}
+
+export default DetailPage;
